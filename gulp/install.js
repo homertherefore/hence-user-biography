eb199ec769086771a18837ec76bdcb942695523a
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-09-23 10:20:54 -0400

    - updating the gulp install to support sudo use when on non-windows machines

--- gulp/install.js
+++ gulp/install.js
@@ -1,11 +1,19 @@
 import gulp from 'gulp';
 import {execSync} from 'child_process';
 
-gulp.task('install-deps', function (done) {
+gulp.task('install-deps', ['install-gems'], function (done) {
   //execSync('npm i -g karma-cli', {stdio: 'inherit'});
   //execSync('npm i', {stdio: 'inherit'});
   execSync('bower i', {stdio: 'inherit'});
-  execSync("gem update && gem install compass susy modular-scale breakpoint scss_lint font-awesome-sass", {stdio: 'inherit'});
+
+  done();
+});
+
+gulp.task('install-gems', function (done) {
+  let isWin = /^win/.test(process.platform);
+  let sudo = isWin ? '' : 'sudo ';
+
+  execSync(`${sudo} gem update && ${sudo} gem install compass susy modular-scale breakpoint scss_lint font-awesome-sass`, {stdio: 'inherit'});
 
   done();
 });
